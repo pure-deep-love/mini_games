58c9cb12bb3390ffebf0a4c6fe1c9df407fb1d8b
pure-deep-love <2654767918@qq.com>
2024-07-31 17:04:36 +0800

    first commit

---
+++ 2048/src/2048.py
@@ -0,0 +1,82 @@
+import numpy as np
+import random
+
+N = max(int(input('input size: ')), 3)
+
+g = np.array([[0 for i in range(N)]for i in range(N)])
+
+def printg():
+    print('-' * 6 * N)
+    for i in range(N):
+        for j in range(N):
+            print(g[i][j], end='\t')
+        if i != N - 1:
+            print(end='\n\n')
+    print()
+    print('-' * 6 * N)
+    
+
+def ran_num():
+    a = random.randint(0, N - 1)
+    b = random.randint(0, N - 1)
+    while(g[a][b]):
+        a = random.randint(0, N - 1)
+        b = random.randint(0, N - 1)      
+    g[a][b] = 2  
+
+def process(arr, st):
+    tmp = np.array([0 for i in range(N)])
+    start, end, step = 0, N, 1
+    j = 0
+    if st == 1:
+        start, end, step = N - 1, -1, -1
+        j = N - 1
+    for i in range(start, end, step):
+        if arr[i] != 0:
+            tmp[j] = arr[i]
+            j = j + 1 if st == 0 else j - 1
+    start, end, step = N - 2, -1, -1
+    if st == 1:
+        start, end, step = 1, N, 1
+    i = start
+    while flag := i > end if st == 0 else i < end:
+        if tmp[i] == tmp[i + 1 if st == 0 else i - 1]:
+            tmp[i] += tmp[i + 1 if st == 0 else i - 1]
+            tmp[i + 1 if st == 0 else i - 1] = 0
+            i = i - 1 if st == 0 else i + 1
+        i = i - 1 if st == 0 else i + 1
+    return tmp
+
+def move(key):
+    for i in range(N):
+        if key == 'w':
+            g[:, i] = process(g[:, i], 0)
+        elif key == 's':
+            g[:, i] = process(g[:, i], 1)
+        elif key == 'a':
+            g[i, :] = process(g[i, :], 0)
+        elif key == 'd':
+            g[i, :] = process(g[i, :], 1)
+
+def check():
+    cnt = 0
+    for i in range(N):
+        for j in range(N):
+            if g[i][j] != 0:
+                cnt += 1
+    if cnt < N * N:
+        return True
+    return False
+
+ran_num(), ran_num()
+printg()
+
+ending = False
+while (not ending) :
+    move(input())
+    if check():
+        ran_num()
+        printg()
+    else:
+        ending = True
+        print("Game Over!")
